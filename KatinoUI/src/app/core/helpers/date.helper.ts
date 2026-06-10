1a625dfdbbbdf7b4ece6a88ca59eb8de92f52ff4
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2026-06-10 23:05:09 +0300

    Fixes for order creation date time filter + show comment even if not custom on order page

--- KatinoUI/src/app/core/helpers/date.helper.ts
+++ KatinoUI/src/app/core/helpers/date.helper.ts
@@ -1,21 +1,25 @@
 export function toUtcStartOfDay(date: Date | null | undefined): string | null {
   if (!date) return null;
   return new Date(
-    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0, 0),
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    0,
+    0,
+    0,
+    0,
   ).toISOString();
 }
 
 export function toUtcEndOfDay(date: Date | null | undefined): string | null {
   if (!date) return null;
   return new Date(
-    Date.UTC(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate(),
-      23,
-      59,
-      59,
-      999,
-    ),
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    23,
+    59,
+    59,
+    999,
   ).toISOString();
 }
